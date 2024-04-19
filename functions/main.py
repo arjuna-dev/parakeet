0258700b39f8e84f5c74bb6c8362214825cd2abe
Arjuna-Dev <arjuna1@protonmail.com>
2024-04-19 11:28:52 +0200

    Typo fixes and added elevenlabs API

--- functions/main.py
+++ functions/main.py
@@ -16,8 +16,6 @@ import os
 import openai
 import json
 
-
-
 @https_fn.on_request(
     cors=options.CorsOptions(
       cors_origins=["*"],
@@ -32,9 +30,9 @@ def parakeetAPI(req: https_fn.Request) -> https_fn.Response:
   topic = request_data.get("topic")
   keywords = request_data.get("keywords")
   native_language = request_data.get("native_language")
-  learning_language = request_data.get("learning_language")
+  target_language = request_data.get("target_language")
 
-  if not all([topic, keywords, native_language, learning_language]):
+  if not all([topic, keywords, native_language, target_language]):
       return {'error': 'Missing required parameters in request data'}
 
     
@@ -47,23 +45,23 @@ def parakeetAPI(req: https_fn.Request) -> https_fn.Response:
       messages=[
           {"role": "system", "content": "You are a helpful assistant."},
           # {"role": "user", "content": f"Give me a json with a name and last name {topic}."},
-          {"role": "user", "content": f'''Please create a JSON file for a conversation for a language learnin app. It should contain 10 sentences, 5 for each character.It should include exactly the following keys for the key value pair in JSON:
+          {"role": "user", "content": f'''Please create a JSON file for a conversation for a language learning app. It should contain 10 sentences, 5 for each character.It should include exactly the following keys for the key value pair in JSON:
 
 title: (Create a title for the conversation.)
-topic: {topic}.
+topic: {topic}.   
 keywords: {keywords}.
 native_language: {native_language}
-learning_language: {learning_language}
+target_language: {target_language}
 conversation: (An array of dictionaries with 10 items. Each dictionary containing these keys:)
 
 - speaker_name: (create a name for each of the speakers)
 - order: (order in the overall sentences)
 - sentence_native: (sentence in {native_language})
-- sentence_transaltion: (sentence in {learning_language})
+- sentence_translation: (sentence in {target_language})
 - keywords_native: (an array of keywords of the sentence in {native_language})
-- keywords_translations: (an array of keywords in {learning_language} with matching index of {native_language} keywords list)
+- keywords_translations: (an array of keywords in {target_language} with matching index of {native_language} keywords list)
 
-For the specifications in parenthesis you must generate the content. The keys shall remain exactly the same.
+For the specifications in parenthesis you must generate the content. The keys shall remain exactly the same. If no keywords are provided add your own keywords to be used in the conversation.
 '''
 }
       ],
