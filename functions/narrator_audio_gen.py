04cfd4b11d49ab632aff44415c42a50fe4bb95a1
Arjuna-Dev <arjuna1@protonmail.com>
2024-05-02 15:10:06 +0200

    Can generate individual files with elabs

--- functions/narrator_audio_gen.py
+++ functions/narrator_audio_gen.py
@@ -4,11 +4,14 @@ import gcloud_text_to_speech_api as gcloud_tts
 
 narrator_voice = gcloud_tts.choose_voice('en-US', "f", "en-US-Standard-C")
 
-with open('narrator_pool.json', 'r') as file:
-    narrator_pool = json.load(file)
+# with open('narrator_pool.json', 'r') as file:
+#     narrator_pool = json.load(file)
 
-for section_key, section in narrator_pool.items():
-    for key, value in section.items():
-        file_name = f"{section_key}_{key}.mp3"
-        # elevenlabs_tts(value, f"audio/{file_name}")
-        gcloud_tts.synthesize_text(value, narrator_voice, f"audio/{file_name}")
+# for section_key, section in narrator_pool.items():
+#     for key, value in section.items():
+#         file_name = f"{section_key}_{key}.mp3"
+#         elevenlabs_tts(value, f"audio/{file_name}")
+#         gcloud_tts.synthesize_text(value, narrator_voice, f"audio/{file_name}")
+
+narrator_voice = "GoZIEyk9z3H2szw545o8" 
+elevenlabs_tts("Text to generate",narrator_voice, f"audio/{"file_name"}.mp3")
