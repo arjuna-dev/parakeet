ad7c611087fe913167d293f2eee59028b6ab164e
Aman Jaiswal <aman.jaiswal@code.berlin>
2024-05-03 15:01:08 +0200

    Add ConfirmScript screen to the project and added form elements to homepage, also showing loading animation

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,4 +1,4 @@
-import 'package:cloud_functions/cloud_functions.dart';
+import 'package:auralearn/screens/confirm_script_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -23,6 +23,11 @@ class _MyHomePageState extends State<HomePage> {
 
   var learningLanguage = '';
 
+  var length = '';
+  var languageLevel = 'A1';
+
+  Map<String, dynamic> script = {};
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,6 +49,7 @@ class _MyHomePageState extends State<HomePage> {
                   });
                 },
               ),
+              const SizedBox(height: 10),
               TextField(
                 decoration: const InputDecoration(
                   border: OutlineInputBorder(),
@@ -55,6 +61,7 @@ class _MyHomePageState extends State<HomePage> {
                   });
                 },
               ),
+              const SizedBox(height: 10),
               TextField(
                 decoration: const InputDecoration(
                   border: OutlineInputBorder(),
@@ -66,6 +73,7 @@ class _MyHomePageState extends State<HomePage> {
                   });
                 },
               ),
+              const SizedBox(height: 10),
               TextField(
                 decoration: const InputDecoration(
                   border: OutlineInputBorder(),
@@ -77,56 +85,93 @@ class _MyHomePageState extends State<HomePage> {
                   });
                 },
               ),
+              const SizedBox(height: 10),
+              TextField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Number of sentences',
+                ),
+                onChanged: (value) {
+                  setState(() {
+                    length = value;
+                  });
+                },
+              ),
+              const SizedBox(height: 10),
+              DropdownButtonFormField<String>(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Language Level',
+                ),
+                value: languageLevel,
+                onChanged: (value) {
+                  setState(() {
+                    languageLevel = value.toString();
+                  });
+                },
+                items: <String>['A1', 'A2', 'B1', 'B2', 'C1', 'C2']
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+              const SizedBox(height: 10),
               FloatingActionButton(
                 onPressed: () async {
-                  print('pressed');
-                  // try {
-                  //   final result = await FirebaseFunctions.instanceFor(
-                  //           region: 'us-central1')
-                  //       .httpsCallable('chatGPT_API_call_0')
-                  //       .call(
-                  //     {
-                  //       "requested_scenario": topic,
-                  //       "keywords": keywords,
-                  //       "native_language": nativeLanguage,
-                  //       "target_language": learningLanguage,
-                  //       "length": "4",
-                  //       "user_ID": "1",
-                  //       "language_level": "A1",
-                  //     },
-                  //   );
-                  //   print(result.data);
-                  // } on FirebaseFunctionsException catch (e) {
-                  //   print('Firebase Functions error: ${e.code}\n${e.details}');
-                  // } catch (e) {
-                  //   print('General error: $e');
-                  // }
-                  final response = await http.post(
-                    Uri.parse(
-                        'https://us-central1-noble-descent-420612.cloudfunctions.net/chatGPT_API_call_0'),
-                    headers: <String, String>{
-                      'Content-Type': 'application/json; charset=UTF-8',
-                      "Access-Control-Allow-Origin":
-                          "*", // Required for CORS support to work
+                  showDialog(
+                    context: context,
+                    barrierDismissible: false,
+                    builder: (BuildContext context) {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
                     },
-                    body: jsonEncode(<String, String>{
-                      "requested_scenario": topic,
-                      "keywords": keywords,
-                      "native_language": nativeLanguage,
-                      "target_language": learningLanguage,
-                      "length": "4",
-                      "user_ID": "1",
-                      "language_level": "A1",
-                    }),
                   );
-                  if (response.statusCode == 200) {
-                    final Map<String, dynamic> data = jsonDecode(response.body);
-                    await FirebaseFirestore.instance
-                        .collection('jsonFiles')
-                        .add(data);
-                    print(data);
-                  } else {
-                    throw Exception('Failed to load API data');
+
+                  try {
+                    final response = await http.post(
+                      Uri.parse(
+                          'https://us-central1-noble-descent-420612.cloudfunctions.net/chatGPT_API_call_0'),
+                      headers: <String, String>{
+                        'Content-Type': 'application/json; charset=UTF-8',
+                        "Access-Control-Allow-Origin":
+                            "*", // Required for CORS support to work
+                      },
+                      body: jsonEncode(<String, String>{
+                        "requested_scenario": topic,
+                        "keywords": keywords,
+                        "native_language": nativeLanguage,
+                        "target_language": learningLanguage,
+                        "length": length,
+                        "user_ID":
+                            FirebaseAuth.instance.currentUser!.uid.toString(),
+                        "language_level": languageLevel,
+                      }),
+                    );
+
+                    if (response.statusCode == 200) {
+                      final Map<String, dynamic> data =
+                          jsonDecode(response.body);
+                      await FirebaseFirestore.instance
+                          .collection('jsonFiles')
+                          .add(data);
+                      print(data);
+                      script = data;
+                    } else {
+                      throw Exception('Failed to load API data');
+                    }
+                  } catch (e) {
+                    print('Error: $e');
+                  } finally {
+                    Navigator.pop(context);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ConfirmScript(script: script),
+                      ),
+                    );
                   }
                 },
                 child: const Icon(Icons.add),
