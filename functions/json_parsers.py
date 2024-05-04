656726845ed8b928c3e06a7c8c88934132d084cb
Arjuna-Dev <arjuna1@protonmail.com>
2024-05-04 13:47:58 +0200

    Refactored according to new jsn format

--- functions/json_parsers.py
+++ functions/json_parsers.py
@@ -12,18 +12,11 @@ class TTS_PROVIDERS(Enum):
     GOOGLE = 1
     ELEVENLABS = 2
 
-def split_words(sentence):
-  additional_chars = '“”‘’—–…«»„©®™£€¥×÷°'
-  punctuation = string.punctuation + additional_chars
-  words = sentence.split()
-  words = [word.strip(punctuation) for word in words]
-  return words
-
 def parse_and_create_script(data):
     script = []
 
     index = random.randint(0, len(sequences.intro_sequences) - 1)
-    intro_sequence = sequences.intro_sequences[index]("title", "target_language")
+    intro_sequence = sequences.intro_sequences[index]()
     script.extend(intro_sequence)
 
     for i, sentence in enumerate(data["dialogue"]):
@@ -45,17 +38,18 @@ def parse_and_create_script(data):
 
         # Process split_sentence items
         for j, split_sentence in enumerate(sentence["split_sentence"]):
-            split_narrator_fun_fact = f"dialogue_{i}_split_sentence_{j}_{"narrator_fun_fact"}"
+            split_narrator_fun_fact = f"dialogue_{i}_split_sentence_{j}_{"narrator_translation"}"
             split_native = f"dialogue_{i}_split_sentence_{j}_{"native_language"}"
             split_target = f"dialogue_{i}_split_sentence_{j}_{"target_language"}"
 
-            words = []
-            for index, value in enumerate(split_words(split_sentence['target_language'])):
-                target_word = f"dialogue_{i}_split_sentence_{j}_target_language_{index}"
-                words.append(target_word)
+            word_objects = []
+            for index, words in enumerate(split_sentence['words']):
+                word = f"dialogue_{i}_split_sentence_{j}_words_{index}_target_language"
+                translation = f"dialogue_{i}_split_sentence_{j}_words_{index}_narrator_translation"
+                word_objects.append({"word": word, "translation": translation})
 
             index = random.randint(0, len(sequences.chunk_sequences) - 1)
-            chunk_sequence = sequences.chunk_sequences[index](split_narrator_fun_fact, split_native, split_target, words)
+            chunk_sequence = sequences.chunk_sequence_3rep_new(split_narrator_fun_fact, split_native, split_target, word_objects)
             script.extend(chunk_sequence)
             print("Chunk sequence: ", index+1)
 
@@ -68,14 +62,11 @@ def language_to_language_code(language):
     else:
         return "Language not found"
 
-def parse_and_convert_to_speech(data, directory, tts_provider):
+def parse_and_convert_to_speech(data, directory, tts_provider, native_language, target_language, metadata):
 
     if tts_provider == TTS_PROVIDERS.GOOGLE.value:
-        target_language = data["target_language"]
-        speaker_1_gender = data["speakers"]["speaker_1"]["gender"].lower()
-        speaker_2_gender = data["speakers"]["speaker_2"]["gender"].lower()
-
-        target_language = data["target_language"]
+        speaker_1_gender = metadata["speakers"]["speaker_1"]["gender"].lower()
+        speaker_2_gender = metadata["speakers"]["speaker_2"]["gender"].lower()
 
         language_code = language_to_language_code(target_language)
 
@@ -92,15 +83,15 @@ def parse_and_convert_to_speech(data, directory, tts_provider):
 
         tts_function = elevenlabs_tts
 
+    title = metadata["title"]
 
     # add a subdirectory to the directory
     os.makedirs(f"{directory}/audio", exist_ok=True)
 
-    text = data["title"]
-    tts_function(text, narrator_voice, f"{directory}/audio/{"title"}.mp3")
+    tts_function(title, narrator_voice, f"{directory}/audio/{"title"}.mp3")
 
     # Process speaker names
-    for speaker_key, speaker_info in data["speakers"].items():
+    for speaker_key, speaker_info in metadata["speakers"].items():
         text = speaker_info["name"]
         speaker = f"speakers_{speaker_key}_name"
         tts_function(text, narrator_voice, f"{directory}/audio/{speaker}.mp3")
@@ -124,8 +115,8 @@ def parse_and_convert_to_speech(data, directory, tts_provider):
 
         # Process split_sentence items
         for j, split_sentence in enumerate(sentence["split_sentence"]):
-            text = split_sentence["narrator_fun_fact"]
-            fun_fact = f"dialogue_{i}_split_sentence_{j}_{"narrator_fun_fact"}"
+            text = split_sentence["narrator_translation"]
+            fun_fact = f"dialogue_{i}_split_sentence_{j}_{"narrator_translation"}"
             tts_function(text, narrator_voice, f"{directory}/audio/{fun_fact}.mp3")
             
             text = split_sentence["native_language"]
@@ -136,7 +127,11 @@ def parse_and_convert_to_speech(data, directory, tts_provider):
             target_chunk = f"dialogue_{i}_split_sentence_{j}_{"target_language"}"
             tts_function(text, current_speaker_voice, f"{directory}/audio/{target_chunk}.mp3")
             
-            for index, value in enumerate(split_words(split_sentence['target_language'])):
-                text = value
-                word = f"dialogue_{i}_split_sentence_{j}_target_language_{index}"
-                tts_function(text, current_speaker_voice, f"{directory}/audio/{word}.mp3")
+            for index, word in enumerate(split_sentence['words']):
+                word_text = word["target_language"]
+                narrator_translation_text = word["narrator_translation"]
+                word_file_name = f"dialogue_{i}_split_sentence_{j}_words_{index}_target_language"
+                narrator_translation_file_name = f"dialogue_{i}_split_sentence_{j}_words_{index}_narrator_translation"
+                tts_function(word_text, current_speaker_voice, f"{directory}/audio/{word_file_name}.mp3")
+                tts_function(narrator_translation_text, narrator_voice, f"{directory}/audio/{narrator_translation_file_name}.mp3")
+
