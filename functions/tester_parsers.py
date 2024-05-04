9c59ed5b2f3abc4d4bba7b113f5c5717b8cb9c16
Arjuna-Dev <arjuna1@protonmail.com>
2024-05-04 14:43:30 +0200

    Added needed arguments

--- functions/tester_parsers.py
+++ functions/tester_parsers.py
@@ -132,9 +132,9 @@ example_dialogue = {"all_turns":[{"native_language":"Albert, I am curious to bet
 os.mkdir(f"other/test_{now}")
 full_lesson_directory = f"other/test_{now}"
 
-directory_for_new_audio = f"audio"
-# parse_and_convert_to_speech(example_JSON, directory_for_new_audio, TTS_PROVIDERS.ELEVENLABS)
+directory_for_new_audio = f"{full_lesson_directory}/audio"
+parse_and_convert_to_speech(example_JSON, directory_for_new_audio, TTS_PROVIDERS.ELEVENLABS.value, "English", "Spanish (Mexico)", example_dialogue)
 script = parse_and_create_script(example_JSON)
 
 print(script)
-# generate_lesson(script, full_lesson_directory, directory_for_new_audio)
+generate_lesson(script, full_lesson_directory, directory_for_new_audio)
