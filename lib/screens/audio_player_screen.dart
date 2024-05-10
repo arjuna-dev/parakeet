75c0bded1938d6a27f862b527e0232cad6f37773
Aman Jaiswal <aman.jaiswal@code.berlin>
2024-05-10 10:52:12 +0200

    Added audio control functionalities

--- lib/screens/audio_player_screen.dart
+++ lib/screens/audio_player_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
+import 'package:rxdart/rxdart.dart';
 
 class AudioPlayerScreen extends StatefulWidget {
-  final List script;
+  final List<dynamic> script;
   final String responseDbId;
 
   const AudioPlayerScreen(
@@ -16,47 +17,63 @@ class AudioPlayerScreen extends StatefulWidget {
 class AudioPlayerScreenState extends State<AudioPlayerScreen> {
   late AudioPlayer player;
   late ConcatenatingAudioSource playlist;
-  bool isPlaying = false;
   String currentTrack = '';
+  bool isPlaying = false;
+  Duration totalDuration = Duration.zero;
+  Duration currentPosition = Duration.zero;
 
   @override
   void initState() {
     super.initState();
     player = AudioPlayer();
+    currentTrack = widget.script[0];
     _initPlaylist();
   }
 
   Future<void> _initPlaylist() async {
-    List<AudioSource> sources = [];
-    try {
-      // Iterate over the script to create sources in order
-      for (var fileName in widget.script) {
-        String fileUrl;
-        if (fileName.startsWith("narrator_") ||
-            fileName == "one_second_break" ||
-            fileName == "five_second_break") {
-          fileUrl =
-              "https://storage.cloud.google.com/narrator_audio_files/google_tts/narrator_english/$fileName.mp3";
-        } else {
-          fileUrl =
-              "https://storage.cloud.google.com/conversations_audio_files/${widget.responseDbId}/$fileName.mp3";
-        }
+    List<AudioSource> sources = widget.script
+        .map((fileName) {
+          String fileUrl = _constructUrl(fileName);
+          if (!Uri.parse(fileUrl).isAbsolute) return null;
+          return ProgressiveAudioSource(Uri.parse(fileUrl));
+        })
+        .whereType<AudioSource>()
+        .toList();
 
-        // Skip iteration if fileUrl is not a valid link
-        if (!Uri.parse(fileUrl).isAbsolute) {
-          continue;
-        }
-        Uri uri = Uri.parse(fileUrl);
-        print('Source URI: $uri');
-        sources.add(ProgressiveAudioSource(Uri.parse(fileUrl)));
+    playlist = ConcatenatingAudioSource(children: sources);
+
+    player.setAudioSource(
+        playlist); // This sets the audio source and prepares it for playback.
+    player.durationStream.listen((duration) {
+      if (duration != null) {
+        setState(() {
+          totalDuration = duration;
+        });
       }
+      print(totalDuration);
+    });
+  }
 
-      playlist = ConcatenatingAudioSource(children: sources);
-      await player.setAudioSource(playlist);
-    } catch (e) {
-      print('Error initializing playlist: $e');
+  String _constructUrl(String fileName) {
+    if (fileName.startsWith("narrator_") ||
+        fileName == "one_second_break" ||
+        fileName == "five_second_break") {
+      return "https://storage.cloud.google.com/narrator_audio_files/google_tts/narrator_english/$fileName.mp3";
+    } else {
+      return "https://storage.cloud.google.com/conversations_audio_files/${widget.responseDbId}/$fileName.mp3";
     }
   }
+
+  Stream<PositionData> get _positionDataStream =>
+      Rx.combineLatest3<Duration, Duration, Duration, PositionData>(
+          player.positionStream,
+          player.bufferedPositionStream,
+          player.durationStream.whereType<Duration>(),
+          (position, bufferedPosition, duration) => PositionData(
+                position,
+                bufferedPosition,
+                duration,
+              ));
 
   @override
   Widget build(BuildContext context) {
@@ -68,24 +85,55 @@ class AudioPlayerScreenState extends State<AudioPlayerScreen> {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
+          StreamBuilder<PositionData>(
+            stream: _positionDataStream,
+            builder: (context, snapshot) {
+              final positionData = snapshot.data;
+              return Column(
+                children: [
+                  if (positionData != null)
+                    Slider(
+                      min: 0.0,
+                      max: positionData.duration.inMilliseconds.toDouble(),
+                      value: positionData.position.inMilliseconds
+                          .clamp(0.0,
+                              positionData.duration.inMilliseconds.toDouble())
+                          .toDouble(),
+                      onChanged: (value) {
+                        player.seek(Duration(milliseconds: value.toInt()));
+                      },
+                    ),
+                  Text(
+                    "${formatDuration(positionData!.position)} / ${formatDuration(positionData.duration)}",
+                  ),
+                ],
+              );
+            },
+          ),
           Text('Now Playing: $currentTrack'),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               IconButton(
-                icon: const Icon(Icons.play_arrow),
-                onPressed: isPlaying ? null : () => _play(),
+                icon: const Icon(Icons.skip_previous),
+                onPressed:
+                    player.hasPrevious ? () => player.seekToPrevious() : null,
               ),
               IconButton(
-                icon: const Icon(Icons.pause),
-                onPressed: isPlaying ? () => _pause() : null,
+                icon: Icon(isPlaying ? Icons.pause : Icons.play_arrow),
+                onPressed: isPlaying ? _pause : _play,
               ),
               IconButton(
                 icon: const Icon(Icons.stop),
-                onPressed: isPlaying ? () => _stop() : null,
+                onPressed: isPlaying ? _stop : null,
+              ),
+              IconButton(
+                icon: const Icon(Icons.skip_next),
+                onPressed: player.hasNext ? () => player.seekToNext() : null,
               ),
             ],
           ),
+          // Control buttons as before
         ],
       ),
     );
@@ -95,10 +143,9 @@ class AudioPlayerScreenState extends State<AudioPlayerScreen> {
     setState(() => isPlaying = true);
     await player.play();
     player.currentIndexStream.listen((index) {
-      if (index != null && index < playlist.children.length) {
+      if (index != null && index < widget.script.length) {
         setState(() {
-          currentTrack = playlist.children[index] as String;
-          print(currentTrack);
+          currentTrack = widget.script[index];
         });
       }
     });
@@ -111,9 +158,10 @@ class AudioPlayerScreenState extends State<AudioPlayerScreen> {
 
   void _stop() {
     player.stop();
+    player.seek(Duration.zero);
     setState(() {
       isPlaying = false;
-      currentTrack = '';
+      currentTrack = widget.script[0];
     });
   }
 
@@ -122,4 +170,19 @@ class AudioPlayerScreenState extends State<AudioPlayerScreen> {
     player.dispose();
     super.dispose();
   }
+}
+
+String formatDuration(Duration d) {
+  String twoDigits(int n) => n.toString().padLeft(2, "0");
+  String twoDigitMinutes = twoDigits(d.inMinutes.remainder(60));
+  String twoDigitSeconds = twoDigits(d.inSeconds.remainder(60));
+  return "${twoDigits(d.inHours)}:$twoDigitMinutes:$twoDigitSeconds";
+}
+
+class PositionData {
+  final Duration position;
+  final Duration bufferedPosition;
+  final Duration duration;
+
+  PositionData(this.position, this.bufferedPosition, this.duration);
 }
