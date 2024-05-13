0e836f911cb1dca7c2cb74a604b00a37e456eea8
Arjuna-Dev <arjuna1@protonmail.com>
2024-05-13 19:03:13 +0200

    Renamed file

---
+++ functions/google_tts_voices.py
@@ -0,0 +1,44 @@
+google_tts_voices =  [
+    {
+      "language": "English",
+      "gender": "m",
+      "voice_id": "en-US-Journey-D",
+      "voice_type": "Premium"
+    },
+    {
+      "language": "English",
+      "gender": "f",
+      "voice_id": "en-US-Journey-F",
+      "voice_type": "Premium"
+    },
+    {
+      "language": "German",
+      "gender": "m",
+      "voice_id": "de-DE-Studio-B",
+      "voice_type": "Premium"
+    },
+    {
+      "language": "German",
+      "gender": "f",
+      "voice_id": "de-DE-Studio-C",
+      "voice_type": "Premium"
+    },
+    {
+      "language": "German",
+      "gender": "m",
+      "voice_id": "de-DE-Neural2-B",
+      "voice_type": "Premium"
+    },
+    {
+      "language": "German",
+      "gender": "f",
+      "voice_id": "de-DE-Neural2-C	",
+      "voice_type": "Premium"
+    },
+    {
+      "language": "Hungarian",
+      "gender": "f",
+      "voice_id": "hu-HU-Wavenet-A",
+      "voice_type": "Premium"
+    }
+  ]
