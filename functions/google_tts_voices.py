f36ba218070da096354e390677c85ab30fb84730
Arjuna-Dev <arjuna1@protonmail.com>
2024-05-19 12:41:21 +0200

    Added categopry

--- functions/google_tts_voices.py
+++ functions/google_tts_voices.py
@@ -18,51 +18,58 @@ google_tts_voices =  [
     # },
     {
       "language": "English",
-      "gender": "m",
-      "voice_id": "en-US-Journey-D",
-      "voice_type": "Premium",
+      "gender": "f",
+      "voice_id": "en-US-Studio-O",
+      "voice_category": "Premium",
+      "voice_type": "Studio",
       "language_code": "en-US"
     },
     {
       "language": "English",
-      "gender": "f",
-      "voice_id": "en-US-Journey-F",
-      "voice_type": "Premium",
+      "gender": "m",
+      "voice_id": "en-US-Studio-Q",
+      "voice_category": "Premium",
+      "voice_type": "Studio",
       "language_code": "en-US"
     },
     {
       "language": "German",
       "gender": "m",
       "voice_id": "de-DE-Studio-B",
-      "voice_type": "Premium",
+      "voice_category": "Premium",
+      "voice_type": "Studio",
       "language_code": "de-DE"
     },
     {
       "language": "German",
       "gender": "f",
       "voice_id": "de-DE-Studio-C",
-      "voice_type": "Premium",
+      "voice_category": "Premium",
+      "voice_type": "Studio",
       "language_code": "de-DE"
     },
     {
       "language": "German",
       "gender": "m",
       "voice_id": "de-DE-Neural2-B",
-      "voice_type": "Premium",
+      "voice_category": "Premium",
+      "voice_type": "Neural2",
       "language_code": "de-DE"
     },
     {
       "language": "German",
       "gender": "f",
       "voice_id": "de-DE-Neural2-C	",
-      "voice_type": "Premium",
+      "voice_category": "Premium",
+      "voice_type": "Neural2",
       "language_code": "de-DE"
     },
     {
       "language": "Hungarian",
       "gender": "f",
       "voice_id": "hu-HU-Wavenet-A",
-      "voice_type": "Premium",
+      "voice_category": "Premium",
+      "voice_type": "Wavenet",
       "language_code": "en-US"
     }
   ]
