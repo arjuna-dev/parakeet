105a250c680e9a3e16f5fdc805e190c3c9808481
Arjuna-Dev <arjuna1@protonmail.com>
2024-05-20 18:53:23 +0200

    Migrated 1st function to a single functions document to avoid repetition allowing for shared scripts

--- functions/utils/chatGPT_API_call.py
+++ functions/utils/chatGPT_API_call.py
@@ -1,14 +1,28 @@
-def chatGPT_API_call(dialogue, native_language, target_language, language_level, length, speakers):
+import openai
+import os
+import json
+from .utilities import is_running_locally, GPT_MODEL
+
+if is_running_locally():
+    from dotenv import load_dotenv
+    load_dotenv()
+    OPEN_AI_API_KEY = os.getenv('OPEN_AI_API_KEY')
+else:
+    OPEN_AI_API_KEY = os.environ.get("OPEN_AI_API_KEY")
+
+assert OPEN_AI_API_KEY, "OPEN_AI_API_KEY is not set in the environment variables"
+
+def chatGPT_API_call(prompt):
 
     client = openai.OpenAI(api_key=OPEN_AI_API_KEY)
 
     # Create the chat completion
     completion = client.chat.completions.create(
-        model=gpt_model,
+        model=GPT_MODEL.GPT_4o.value,
         #   stream=True,
         messages=[
             {"role": "system", "content": "You are a language learning teacher and content creator. You specialize in creating engaging conversations in any language to be used as content for learning. You are also able to create conversations in different tones and for different audiences."},
-            {"role": "user", "content": prompt(dialogue, native_language, target_language, language_level, length, speakers)}
+            {"role": "user", "content": prompt}
         ],
         response_format={'type': 'json_object'}
     )
@@ -20,6 +34,6 @@ def chatGPT_API_call(dialogue, native_language, target_language, language_level,
         print(chatGPT_JSON_response)
         print(f"Error parsing JSON response from chatGPT: {e}")
         #TODO: log error and failed JSON in DB and ask the user to try again
-        return
+        raise Exception("Error parsing JSON response from chatGPT")
 
     return data
