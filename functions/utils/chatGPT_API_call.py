dc14b23f1138a43cc7b895f17fea2640e7d6ddcc
Arjuna-Dev <arjuna1@protonmail.com>
2024-05-23 18:22:14 +0200

    Enabled stream with argument

--- functions/utils/chatGPT_API_call.py
+++ functions/utils/chatGPT_API_call.py
@@ -12,14 +12,14 @@ else:
 
 assert OPEN_AI_API_KEY, "OPEN_AI_API_KEY is not set in the environment variables"
 
-def chatGPT_API_call(prompt):
+def chatGPT_API_call(prompt, use_stream):
 
     client = openai.OpenAI(api_key=OPEN_AI_API_KEY)
 
     # Create the chat completion
     completion = client.chat.completions.create(
         model=GPT_MODEL.GPT_4o.value,
-        #   stream=True,
+        stream=use_stream,
         messages=[
             {"role": "system", "content": "You are a language learning teacher and content creator. You specialize in creating engaging conversations in any language to be used as content for learning. You are also able to create conversations in different tones and for different audiences."},
             {"role": "user", "content": prompt}
