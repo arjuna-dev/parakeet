7676517951cf0a7d172e9455fccee5bc668dc27f
Aman Jaiswal <aman.jaiswal@code.berlin>
2024-05-25 19:23:02 +0200

    updated frontend logic to display confirm dialogue screen correctly with the stream implementation

--- lib/screens/confirm_dialogue_screen.dart
+++ lib/screens/confirm_dialogue_screen.dart
@@ -30,8 +30,8 @@ class ConfirmDialogue extends StatefulWidget {
 }
 
 class _ConfirmDialogueState extends State<ConfirmDialogue> {
-  // Map<String, dynamic> script = {};
-  // Map<int, Map<String, bool>> selectedWords = {};
+  Map<String, dynamic> script = {};
+  Map<int, Map<String, bool>> selectedWords = {};
 
   @override
   void initState() {
@@ -65,15 +65,170 @@ class _ConfirmDialogueState extends State<ConfirmDialogue> {
             return const CircularProgressIndicator();
           }
 
-          return ListView(
-            children: snapshot.data!.docs.map((DocumentSnapshot document) {
-              Map<String, dynamic> data =
-                  document.data() as Map<String, dynamic>;
-              return ListTile(
-                title: Text(data["target"] as String),
-                // Add more fields as needed
-              );
-            }).toList(),
+          return Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          Column(
+                            children: snapshot.data!.docs
+                                .map((DocumentSnapshot document) {
+                              Map<String, dynamic> data =
+                                  document.data() as Map<String, dynamic>;
+                              List<dynamic> turns = [];
+                              if (data.containsKey("all_turns")) {
+                                turns = data["all_turns"];
+                              }
+
+                              return ListView.builder(
+                                itemBuilder: (context, index) {
+                                  if (index >= turns.length) {
+                                    return Container(); // Return an empty container if index is out of bounds
+                                  }
+                                  final turn = turns[index];
+                                  final targetLanguageSentence =
+                                      turn['target_language'] ?? "";
+                                  final words =
+                                      targetLanguageSentence?.split(' ');
+                                  words.forEach((word) {
+                                    if (selectedWords[index] == null) {
+                                      selectedWords[index] = {};
+                                    }
+                                    if (selectedWords[index]![word] == null) {
+                                      selectedWords[index]![word] = true;
+                                    }
+                                  });
+
+                                  return ListTile(
+                                    title: Text(
+                                        'Sentence Number: ${turn['turn_nr']}'),
+                                    subtitle: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(turn['native_language'] ??
+                                            "No native language"),
+                                        Wrap(
+                                          children: words.map<Widget>((word) {
+                                            return Row(
+                                              mainAxisSize: MainAxisSize.min,
+                                              children: [
+                                                Checkbox(
+                                                  value: selectedWords[index]![
+                                                      word],
+                                                  onChanged: (bool? value) {
+                                                    setState(() {
+                                                      selectedWords[index]![
+                                                          word] = value!;
+                                                    });
+                                                  },
+                                                ),
+                                                Text(word),
+                                              ],
+                                            );
+                                          }).toList(),
+                                        ),
+                                      ],
+                                    ),
+                                  );
+                                },
+                                itemCount: turns.length,
+                                shrinkWrap: true,
+                                physics: const NeverScrollableScrollPhysics(),
+                              );
+                            }).toList(),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  FloatingActionButton(
+                    onPressed: () async {
+                      print("confirmed");
+                      showDialog(
+                        context: context,
+                        barrierDismissible: false,
+                        builder: (BuildContext context) {
+                          return const Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        },
+                      );
+
+                      try {
+                        final response = await http.post(
+                          Uri.parse(
+                              'https://europe-west1-noble-descent-420612.cloudfunctions.net/full_API_workflow'), // need the function url here
+                          headers: <String, String>{
+                            'Content-Type': 'application/json; charset=UTF-8',
+                            "Access-Control-Allow-Origin":
+                                "*", // Required for CORS support to work
+                          },
+                          body: jsonEncode(<String, dynamic>{
+                            "response_db_id": widget.dialogue["response_db_id"],
+                            "dialogue": widget.dialogue["all_turns"],
+                            "title": widget.dialogue["title"],
+                            "speakers": widget.dialogue["speakers"],
+                            "user_ID": widget.dialogue["user_ID"],
+                            "native_language": widget.nativeLanguage,
+                            "target_language": widget.targetLanguage,
+                            "length": widget.length,
+                            "language_level": widget.languageLevel,
+                            "words_to_repeat": selectedWords.entries
+                                .expand((entry) => entry.value.entries)
+                                .where((innerEntry) => innerEntry.value == true)
+                                .map((innerEntry) => innerEntry.key)
+                                .toList(),
+                          }),
+                        );
+
+                        if (response.statusCode == 200) {
+                          final Map<String, dynamic> data =
+                              jsonDecode(response.body);
+                          print(data);
+                          script = data;
+
+                          if (script.isNotEmpty &&
+                              script.containsKey('script')) {
+                            Navigator.pop(context);
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => AudioPlayerScreen(
+                                        script: script['script'],
+                                        dialogue: widget.dialogue["all_turns"],
+                                        responseDbId:
+                                            widget.dialogue["response_db_id"],
+                                        userID: FirebaseAuth
+                                            .instance.currentUser!.uid,
+                                        title: script['title'],
+                                        audioDurations: script['fileDurations'],
+                                      )),
+                            );
+                          } else {
+                            throw Exception(
+                                'Proper data not received from API');
+                          }
+                        } else {
+                          throw Exception('Failed to load API data');
+                        }
+                      } catch (e) {
+                        Navigator.pop(context);
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content:
+                                Text('Something went wrong! Please try again.'),
+                            duration: Duration(seconds: 4),
+                          ),
+                        );
+                      }
+                    },
+                    child: const Text('Confirm'),
+                  )
+                ]),
           );
         },
       ),
