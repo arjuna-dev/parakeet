42dd8d872c309b45c83be6340ccdfea661300fb0
Arjuna-Dev <arjuna1@protonmail.com>
2024-05-31 20:50:27 +0200

    Specified how to denote sex

--- functions/utils/prompts.py
+++ functions/utils/prompts.py
@@ -10,7 +10,7 @@ target_language: {target_language}
 native_language: {native_language}
 language_level: {language_level}
 
-The keywords should be used in the dialogue if they are provided. If there are spelling mistakes in the content request, fix them. The title should be in {native_language}. The names of the speakers should be matching the speakers mentioned in the requested scenario, if no names are provided use the target_language language and culture to create the names. The main original dialogue happens in {target_language}, the translations to {native_language} should be as literal as possible. Skip introductions between speakers unless specified and go straight to the topic of conversation.
+The keywords should be used in the dialogue if they are provided. If there are spelling mistakes in the content request, fix them. The title should be in {native_language}. The names of the speakers should be matching the speakers mentioned in the requested scenario, if no names are provided use the target_language language and culture to create the names. The main original dialogue happens in {target_language}, the translations to {native_language} should be as literal as possible. Skip introductions between speakers unless specified and go straight to the topic of conversation. Specify gender with "m" for male and "f" for female.
 
 With the following data as an example enclosed in double vertical lines (||):
 
