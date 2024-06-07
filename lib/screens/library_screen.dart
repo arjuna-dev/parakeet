53ec767fc75149b8ad0a71e15d3270ea89f6605d
Aman Jaiswal <aman.jaiswal@code.berlin>
2024-06-07 12:36:33 +0200

    Fixed the typo

--- lib/screens/library_screen.dart
+++ lib/screens/library_screen.dart
@@ -98,7 +98,7 @@ class _LibraryState extends State<Library> {
 
                                       // Remove the audio file from Firestore
                                       await userDocRef.update({
-                                        'FavoriteAudioFiles':
+                                        'favoriteAudioFiles':
                                             FieldValue.arrayRemove([
                                           {'parentId': parentId, 'docId': docId}
                                         ])
@@ -109,7 +109,7 @@ class _LibraryState extends State<Library> {
 
                                       // Add the audio file to Firestore
                                       await userDocRef.set({
-                                        'FavoriteAudioFiles':
+                                        'favoriteAudioFiles':
                                             FieldValue.arrayUnion([
                                           {'parentId': parentId, 'docId': docId}
                                         ])
