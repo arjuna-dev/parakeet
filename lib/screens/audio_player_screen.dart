339f336636341100e67ede8764d30955befa85c7
Arjuna-Dev <arjuna1@protonmail.com>
2024-06-20 10:32:02 +0200

    Import fix

--- lib/screens/audio_player_screen.dart
+++ lib/screens/audio_player_screen.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-import 'package:parakeet/screens/home_screen.dart';
+import 'package:parakeet/services/home_screen_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
