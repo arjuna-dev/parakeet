afa9e7dee02362802a3bd04f88f3a869bb841824
Aman Jaiswal <aman.jaiswal@code.berlin>
2024-06-23 16:57:25 +0200

    Fixed the closing phrase not repeating after completion of every sentence

--- lib/utils/script_generator.dart
+++ lib/utils/script_generator.dart
@@ -165,9 +165,12 @@ List<String> parseAndCreateScript(
         // }
       }
     }
+    if (i == dialogue.length - 1) {
+      Random random = Random();
+      int randomNumber = random.nextInt(5);
+      script.add("narrator_closing_phrases_$randomNumber");
+    }
   }
-  Random random = Random();
-  int randomNumber = random.nextInt(5);
-  script.add("narrator_closing_phrases_$randomNumber");
+
   return script;
 }
