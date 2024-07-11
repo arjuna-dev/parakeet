9272e0585ec2cbcaaef9a5a5fa31150358961d79
arjun-dev <arjuna1@protonmail.com>
2024-07-11 17:07:25 +0200

    Using llm-parametrizer

--- functions/utils/local_scripts/_local_tester_chatGPT_dialogue.py
+++ functions/utils/local_scripts/_local_tester_chatGPT_dialogue.py
@@ -5,25 +5,29 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from prompts import prompt_dialogue
 import chatGPT_API_call
+from llm_parametrizer import LLMParametrizer
 
-now = datetime.datetime.now().strftime("%m.%d.%H.%M.%S")
+# user = input("Enter the user name: ")
+# requested_scenario = input("Requested scenario: ")
+# native_language = input("Native language: ")
+# target_language = input("Target language: ")
+# language_level = input("Language level: ")
+# length = input("Length: ")
+# keywords = input("Keywords: ")
+user = "Arjuna"
+requested_scenario = "Mother complimenting cereal selection"
+native_language = "English (UK)"
+target_language = "German"
+language_level = "A1"
+length = "4"
+keywords = "shark, roller coaster, ants, ping-pong, shaquille oneal, heavy investments"
 
-user = input("Enter the user name: ")
-requested_scenario = input("Requested scenario: ")
-native_language = input("Native language: ")
-target_language = input("Target language: ")
-language_level = input("Language level: ")
-length = input("Length: ")
-keywords = input("Keywords: ")
+prompt1 = prompt_dialogue(requested_scenario, native_language, target_language, "Absolute Beginner", keywords, length)
+prompt2 = prompt_dialogue(requested_scenario, native_language, target_language, "Intermediate", keywords, length)
+prompt3 = prompt_dialogue(requested_scenario, native_language, target_language, "Advanced", keywords, length)
 
-prompt = prompt_dialogue(requested_scenario, native_language, target_language, language_level, "user_ID", length, keywords)
-response = chatGPT_API_call(prompt)
-
-print("response: ",response)
-
-# Create directory
-directory = f"other/{user}_{now}"
-os.makedirs(directory, exist_ok=True)
-
-with open(f"{directory}/chatGPT_response.json", "w") as file:
-    json.dump(response, file)
+prmtrzr = LLMParametrizer()
+prmtrzr.initialize_OpenAI()
+prmtrzr.add_prompts(prompt1, prompt2, prompt3)
+results = prmtrzr.run(output_json=True)
+print(results)
