ee6f3c8a7ffe5d9be49b3994333750b4a738b431
Aman Jaiswal <aman.jaiswal@code.berlin>
2024-07-17 12:49:05 +0200

    Added in_app_purchase package and neccessary dummy screens

---
+++ lib/screens/store_view.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class StoreView extends StatefulWidget {
+  const StoreView({super.key});
+
+  @override
+  State<StoreView> createState() => _StoreViewState();
+}
+
+class _StoreViewState extends State<StoreView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Store'),
+      ),
+      body: const SafeArea(
+          child: Column(
+        children: <Widget>[
+          Text('Welcome'),
+        ],
+      )),
+    );
+  }
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -13,6 +13,7 @@ import firebase_auth
 import firebase_core
 import firebase_storage
 import google_sign_in_ios
+import in_app_purchase_storekit
 import just_audio
 import path_provider_foundation
 import shared_preferences_foundation
@@ -26,6 +27,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  InAppPurchasePlugin.register(with: registry.registrar(forPlugin: "InAppPurchasePlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
