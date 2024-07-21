df055a4e501a42e5d7a8e4ac763c420d1a768fbe
Aman Jaiswal <aman.jaiswal@code.berlin>
2024-07-21 15:50:09 +0200

    added pubsub topic and fixed the errors

--- payment_verification_backend/src/constants.ts
+++ payment_verification_backend/src/constants.ts
@@ -1,4 +1,4 @@
-export const APP_STORE_SHARED_SECRET = "";
+export const APP_STORE_SHARED_SECRET = "ahdghasoghrsg"; // dummy secret
 export const CLOUD_REGION = "europe-west1";
 export const ANDROID_PACKAGE_ID = "com.parakeetapp.app";
-export const GOOGLE_PLAY_PUBSUB_TOPIC = "";
+export const GOOGLE_PLAY_PUBSUB_TOPIC = "parakeet_purchases";
--- payment_verification_backend/src/firebase.calls.ts
+++ payment_verification_backend/src/firebase.calls.ts
@@ -1,4 +1,4 @@
-import firestore from "firebase-admin/lib/firestore";
+import {firestore} from "firebase-admin";
 import Timestamp = firestore.Timestamp;
 
 export type SubscriptionStatus = "PENDING" | "ACTIVE" | "EXPIRED";
