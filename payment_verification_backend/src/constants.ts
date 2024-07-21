4ba51b1109f3db7485bc117d6eae5b36deba5f88
Aman Jaiswal <aman.jaiswal@code.berlin>
2024-07-21 14:57:11 +0200

    replaced python function to typescript function to be consistent with tutorial

---
+++ payment_verification_backend/src/constants.ts
@@ -0,0 +1,4 @@
+export const APP_STORE_SHARED_SECRET = "";
+export const CLOUD_REGION = "europe-west1";
+export const ANDROID_PACKAGE_ID = "com.parakeetapp.app";
+export const GOOGLE_PLAY_PUBSUB_TOPIC = "";
