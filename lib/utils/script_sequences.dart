efafc03190aa24424c4e2ebee3e5727d78b9e3e6
arjun-dev <arjuna1@protonmail.com>
2024-10-27 14:10:53 +0100

    Added placeholder files to signal which file should be repeated. Removed horrible 11labs fixes

--- lib/utils/script_sequences.dart
+++ lib/utils/script_sequences.dart
@@ -1,8 +1,4 @@
-List<Function> introSequences = [
-  introSequence1,
-  introSequence3,
-  introSequence4
-];
+List<Function> introSequences = [introSequence1, introSequence3, introSequence4];
 
 List<String> introSequence1() {
   List<String> scriptPart = [
@@ -48,12 +44,8 @@ List<String> introOutroSequence1() {
 }
 
 List<Function> sentenceSequences = [sentenceSequence1];
-List<String> sentenceSequence1(String native, String target,
-    String narratorExplanation, List<String> narratorFunFact,
-    {bool isFirstSentence = false}) {
-  String firstPhrase = isFirstSentence
-      ? "narrator_navigation_phrases_20"
-      : "narrator_navigation_phrases_21";
+List<String> sentenceSequence1(String native, String target, String narratorExplanation, List<String> narratorFunFact, {bool isFirstSentence = false}) {
+  String firstPhrase = isFirstSentence ? "narrator_navigation_phrases_20" : "narrator_navigation_phrases_21";
   List<String> scriptPart = [
     firstPhrase, //For now just listen
     "one_second_break",
@@ -76,28 +68,23 @@ List<String> activeRecallSequence1(String native, String target) {
     "narrator_navigation_phrases_8_0", // do you remember how to say...
     "one_second_break",
     native,
+    '\$$target',
     "five_second_break",
     target,
+    '\$$target',
     "five_second_break",
     target,
+    '\$$target',
     "five_second_break",
   ];
   return scriptPart;
 }
 
 List<Function> chunkSequences = [chunkSequence1];
-List<String> chunkSequence1(
-    List<String> narratorTranslationsChunk,
-    String nativeLanguage,
-    String targetLanguage,
-    List<Map<String, dynamic>> wordObjects,
-    int chunkNumber) {
-  List<String> allWordsRepetitions =
-      words2RepsFixElevenlabsLonelyWords(wordObjects);
+List<String> chunkSequence1(List<String> narratorTranslationsChunk, String nativeLanguage, String targetLanguage, List<Map<String, dynamic>> wordObjects, int chunkNumber) {
+  List<String> allWordsRepetitions = words2Reps(wordObjects);
   //List<String> chunkSpaced = spacedWordsFixElevenlabsLonelyWords(wordObjects);
-  String firstPhrase = chunkNumber == 0
-      ? "narrator_navigation_phrases_17"
-      : "narrator_navigation_phrases_23";
+  String firstPhrase = chunkNumber == 0 ? "narrator_navigation_phrases_17" : "narrator_navigation_phrases_23";
   List<String> scriptPart = [
     firstPhrase,
     "narrator_navigation_phrases_22", // Just listen
@@ -108,27 +95,31 @@ List<String> chunkSequence1(
     "narrator_navigation_phrases_15", // Now try to say..
     "one_second_break",
     nativeLanguage,
+    '\$$targetLanguage',
     "five_second_break",
     //...chunkSpaced,
     targetLanguage,
+    '\$$targetLanguage',
     "five_second_break",
     "narrator_repetition_phrases_25", // Pay attention to the pronunciation and try saying it just like that.
     "one_second_break",
     targetLanguage,
+    '\$$targetLanguage',
     "five_second_break",
     ...narratorTranslationsChunk,
     "narrator_repetition_phrases_4", //Listen and repeat
     "one_second_break",
     targetLanguage,
+    '\$$targetLanguage',
     "five_second_break",
     targetLanguage,
+    '\$$targetLanguage',
     "five_second_break"
   ];
   return scriptPart;
 }
 
-List<String> words2RepsFixElevenlabsLonelyWords(
-    List<Map<String, dynamic>> wordObjects) {
+List<String> words2Reps(List<Map<String, dynamic>> wordObjects) {
   List<String> scriptPart = [];
   for (int i = 0; i < wordObjects.length; i++) {
     scriptPart.addAll(wordObjects[i]["translation"]);
@@ -137,19 +128,23 @@ List<String> words2RepsFixElevenlabsLonelyWords(
       scriptPart.add("narrator_repetition_phrases_4"); // Listen and repeat
       scriptPart.add("one_second_break");
     }
-    scriptPart.add(wordObjects[i]["translation"][0]);
+    String word = wordObjects[i]["word"];
+    scriptPart.add(word);
+    scriptPart.add('\$$word');
     scriptPart.add("five_second_break");
-    scriptPart.add(wordObjects[i]["translation"][0]);
+    scriptPart.add(word);
+    scriptPart.add('\$$word');
     scriptPart.add("five_second_break");
   }
   return scriptPart;
 }
 
-List<String> spacedWordsFixElevenlabsLonelyWords(
-    List<Map<String, dynamic>> wordObjects) {
+List<String> spacedWords(List<Map<String, dynamic>> wordObjects) {
   List<String> scriptPart = [];
   for (int i = 0; i < wordObjects.length; i++) {
-    scriptPart.add(wordObjects[i]["translation"][0]);
+    String word = wordObjects[i]["word"];
+    scriptPart.add(word);
+    scriptPart.add('\$$word');
     scriptPart.add("one_second_break");
   }
   return scriptPart;
