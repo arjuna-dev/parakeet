fedc3f39678556befa9d8bc4b8bf5d41a7379a87
arjun-dev <arjuna1@protonmail.com>
2025-01-09 14:19:06 +0100

    play-pause refactoring and  repetitionsMode out-of-bounds handling

--- lib/screens/audio_player_screen.dart
+++ lib/screens/audio_player_screen.dart
@@ -69,9 +69,7 @@ class AudioPlayerScreenState extends State<AudioPlayerScreen> {
   String? targetPhraseToCompareWith;
   String? voiceLanguageCode;
   bool isLanguageSupported = false;
-  bool isPlaying = false;
-  bool isStopped = false;
-  bool _isPaused = false;
+  final ValueNotifier<bool> isPlaying = ValueNotifier<bool>(false);
   int updateNumber = 0;
   bool speechRecognitionActive = false;
   bool? speechRecognitionSupported;
@@ -109,7 +107,7 @@ class AudioPlayerScreenState extends State<AudioPlayerScreen> {
   final StreakService _streakService = StreakService();
   bool _showStreak = false;
 
-  final ValueNotifier<RepetitionMode> _repetitionMode = ValueNotifier(RepetitionMode.normal);
+  final ValueNotifier<RepetitionMode> _repetitionsMode = ValueNotifier(RepetitionMode.normal);
 
   @override
   void initState() {
@@ -123,7 +121,7 @@ class AudioPlayerScreenState extends State<AudioPlayerScreen> {
     analyticsManager.loadAnalyticsFromFirebase();
     _listenToPlayerStreams();
     cachedAudioDurations = getAudioDurationsFromNarratorStorage();
-    firestoreService = UpdateFirestoreService.getInstance(widget.documentID, widget.generating, updatePlaylist, updateTrack, saveSnapshot);
+    firestoreService = UpdateFirestoreService.getInstance(widget.documentID, widget.generating, updatePlaylist, updateTrackLength, saveSnapshot);
     fileDurationUpdate = FileDurationUpdate.getInstance(widget.documentID, calculateTotalDurationAndUpdateTrackDurations);
     getExistingBigJson();
     updateHasNicknameAudio().then((_) {
@@ -147,8 +145,15 @@ class AudioPlayerScreenState extends State<AudioPlayerScreen> {
         }
       },
     );
-    _repetitionMode.addListener(() {
+    _repetitionsMode.addListener(() {
       updatePlaylistOnTheFly();
+    });
+    isPlaying.addListener(() async {
+      if (isPlaying.value) {
+        await _play();
+      } else {
+        await _pause();
+      }
     });
   }
 
@@ -276,22 +281,14 @@ class AudioPlayerScreenState extends State<AudioPlayerScreen> {
     for (int i = 1; i <= retries; i++) {
       bool exists = await urlExists(url);
       if (exists) {
-        if (mounted) {
-          setState(() {
-            isLoading = false;
-          });
-        } else {
-          print('State not mounted for isLoading');
-        }
+        setState(() {
+          isLoading = false;
+        });
         return true;
       }
-      if (mounted) {
-        setState(() {
-          isLoading = true;
-        });
-      } else {
-        print('State not mounted for isLoading');
-      }
+      setState(() {
+        isLoading = true;
+      });
       if (i == 2) {
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(
@@ -345,7 +342,7 @@ class AudioPlayerScreenState extends State<AudioPlayerScreen> {
   void _listenToPlayerStreams() {
     player.playerStateStream.listen((playerState) {
       if (playerState.processingState == ProcessingState.completed) {
-        if (isPlaying) {
+        if (isPlaying.value) {
           analyticsManager.storeAnalytics(widget.documentID, 'completed');
           _handleLessonCompletion();
         }
@@ -398,7 +395,8 @@ class AudioPlayerScreenState extends State<AudioPlayerScreen> {
         return null;
       });
 
-      await _play();
+      await player.playerStateStream.where((state) => state.processingState == ProcessingState.ready).first;
+      isPlaying.value = true;
     } else {
       print("No valid URLs available to initialize the playlist.");
     }
@@ -430,7 +428,7 @@ class AudioPlayerScreenState extends State<AudioPlayerScreen> {
 
   void updatePlaylist(snapshot) async {
     try {
-      script = script_generator.parseAndCreateScript(snapshot.docs[0].data()["dialogue"] as List<dynamic>, widget.wordsToRepeat, widget.dialogue, _repetitionMode);
+      script = script_generator.parseAndCreateScript(snapshot.docs[0].data()["dialogue"] as List<dynamic>, widget.wordsToRepeat, widget.dialogue, _repetitionsMode);
     } catch (e) {
       print("Error parsing and creating script: $e");
       return;
@@ -453,14 +451,20 @@ class AudioPlayerScreenState extends State<AudioPlayerScreen> {
 
     final newTracks = fileUrls.where((url) => url.isNotEmpty).map((url) => AudioSource.uri(Uri.parse(url))).toList();
     await playlist.addAll(newTracks);
-    if (!widget.generating) {
-      await _play();
-    }
+    // if (!widget.generating && !isPlaying.value) {
+    // isPlaying.value = true;
+    // }
 
     updateNumber++;
   }
 
   void updatePlaylistOnTheFly() async {
+    bool wasPlaying = isPlaying.value;
+    int lastIndexBeforeUpdate = player.currentIndex!;
+    if (!widget.generating) {
+      isPlaying.value = false;
+    }
+
     if (widget.generating) {
       print("Error: updatePlaylistOnTheFly() called while generating.");
       return;
@@ -472,7 +476,7 @@ class AudioPlayerScreenState extends State<AudioPlayerScreen> {
     }
 
     try {
-      script = script_generator.parseAndCreateScript(existingBigJson!["dialogue"] as List<dynamic>, widget.wordsToRepeat, widget.dialogue, _repetitionMode);
+      script = script_generator.parseAndCreateScript(existingBigJson!["dialogue"] as List<dynamic>, widget.wordsToRepeat, widget.dialogue, _repetitionsMode);
     } catch (e) {
       print("Error parsing and creating script: $e");
       return;
@@ -484,6 +488,12 @@ class AudioPlayerScreenState extends State<AudioPlayerScreen> {
 
     var newScript = List.from(script);
 
+    // Check if lastIndexBeforeUpdate is within the range of newScript
+    if (lastIndexBeforeUpdate >= newScript.length) {
+      await player.seek(Duration.zero, index: newScript.length - 10);
+      _savePlayerPosition();
+    }
+
     // Filter out files that start with a '$'
     newScript = newScript.where((fileName) => !fileName.startsWith('\$')).toList();
 
@@ -493,12 +503,13 @@ class AudioPlayerScreenState extends State<AudioPlayerScreen> {
     }
 
     final newTracks = fileUrls.where((url) => url.isNotEmpty).map((url) => AudioSource.uri(Uri.parse(url))).toList();
+
     await playlist.clear();
     await playlist.addAll(newTracks);
-    if (!widget.generating) {
-      await _play();
+    await updateTrackLength();
+    if (!widget.generating && wasPlaying) {
+      isPlaying.value = true;
     }
-    updateTrack();
   }
 
   void saveSnapshot(QuerySnapshot snapshot) {
@@ -507,7 +518,7 @@ class AudioPlayerScreenState extends State<AudioPlayerScreen> {
     }
   }
 
-  Future<void> updateTrack() async {
+  Future<void> updateTrackLength() async {
     CollectionReference colRef = FirebaseFirestore.instance.collection('chatGPT_responses').doc(widget.documentID).collection('file_durations');
     QuerySnapshot querySnap = await colRef.get();
     if (querySnap.docs.isNotEmpty) {
@@ -546,7 +557,7 @@ class AudioPlayerScreenState extends State<AudioPlayerScreen> {
   Stream<PositionData> get _positionDataStream {
     int lastIndex = -1;
     return Rx.combineLatest3<Duration, Duration, int, PositionData?>(
-      player.positionStream.where((_) => !_isPaused),
+      player.positionStream.where((_) => isPlaying.value),
       player.durationStream.whereType<Duration>(),
       player.currentIndexStream.whereType<int>().startWith(0),
       (position, duration, index) {
@@ -591,13 +602,9 @@ class AudioPlayerScreenState extends State<AudioPlayerScreen> {
     }
 
     if (updateNumber == widget.dialogue.length || !widget.generating) {
-      calculateFinalTotalDuration();
+      finalTotalDuration = trackDurations.fold(Duration.zero, (total, d) => total + d);
+      setState(() {});
     }
-  }
-
-  void calculateFinalTotalDuration() {
-    finalTotalDuration = trackDurations.fold(Duration.zero, (total, d) => total + d);
-    setState(() {});
   }
 
   Future<Map<String, dynamic>> getAudioDurationsFromNarratorStorage() async {
@@ -671,8 +678,6 @@ class AudioPlayerScreenState extends State<AudioPlayerScreen> {
         Future.delayed(const Duration(milliseconds: 4500), () => _compareSpeechWithPhrase(stringWhenStarting));
       } else {
         if (voskSpeechService == null) {
-          print('voskSpeechService is null in _handleTrackChangeToCompareSpeech');
-          print('voskSpeechService is null in _handleTrackChangeToCompareSpeech');
           print('voskSpeechService is null in _handleTrackChangeToCompareSpeech');
           return;
         }
@@ -756,9 +761,9 @@ class AudioPlayerScreenState extends State<AudioPlayerScreen> {
   }
 
   void _compareSpeechWithPhrase([String? stringWhenStarting]) async {
-    if (isPlaying == false || isStopped == true) {
-      print("Brooooke!");
-      // return;
+    if (!isPlaying.value) {
+      print("Returning early because player was paused");
+      return;
     }
     if (targetPhraseToCompareWith != null && !isSliderMoving) {
       String normalizedLiveTextSpeechToText = _normalizeString(liveTextSpeechToText);
@@ -897,10 +902,10 @@ class AudioPlayerScreenState extends State<AudioPlayerScreen> {
               if (didPop) return;
               final NavigatorState navigator = Navigator.of(context);
               if (!widget.generating) {
-                if (!isStopped && isPlaying) await _pause();
+                if (isPlaying.value) await _pause();
                 navigator.pop('reload');
               } else {
-                if (!isStopped && isPlaying) await _pause();
+                if (isPlaying.value) await _pause();
                 navigator.popUntil((route) => route.isFirst);
                 navigator.pushReplacementNamed('/');
               }
@@ -948,7 +953,7 @@ class AudioPlayerScreenState extends State<AudioPlayerScreen> {
                           positionDataStream: _positionDataStream,
                           totalDuration: totalDuration,
                           finalTotalDuration: finalTotalDuration,
-                          isPlaying: isPlaying,
+                          isPlaying: isPlaying.value,
                           savedPosition: savedPosition,
                           findTrackIndexForPosition: findTrackIndexForPosition,
                           player: player,
@@ -978,9 +983,16 @@ class AudioPlayerScreenState extends State<AudioPlayerScreen> {
                                       icon: const Icon(Icons.skip_previous),
                                       onPressed: player.hasPrevious ? () => player.seekToPrevious() : null,
                                     ),
-                                    IconButton(
-                                      icon: Icon(isPlaying ? Icons.pause : Icons.play_arrow),
-                                      onPressed: isPlaying ? () => _pause() : _play,
+                                    ValueListenableBuilder<bool>(
+                                      valueListenable: isPlaying,
+                                      builder: (context, playing, child) {
+                                        return IconButton(
+                                          icon: Icon(playing ? Icons.pause : Icons.play_arrow),
+                                          onPressed: () {
+                                            isPlaying.value = !isPlaying.value;
+                                          },
+                                        );
+                                      },
                                     ),
                                     IconButton(
                                       icon: const Icon(Icons.skip_next),
@@ -1030,7 +1042,7 @@ class AudioPlayerScreenState extends State<AudioPlayerScreen> {
                                           child: Row(
                                             children: [
                                               const Text('Normal Repetitions'),
-                                              if (_repetitionMode.value == RepetitionMode.normal)
+                                              if (_repetitionsMode.value == RepetitionMode.normal)
                                                 const Padding(
                                                   padding: EdgeInsets.only(left: 8.0),
                                                   child: Icon(Icons.check, size: 18),
@@ -1043,7 +1055,7 @@ class AudioPlayerScreenState extends State<AudioPlayerScreen> {
                                           child: Row(
                                             children: [
                                               const Text('Less Repetitions'),
-                                              if (_repetitionMode.value == RepetitionMode.less)
+                                              if (_repetitionsMode.value == RepetitionMode.less)
                                                 const Padding(
                                                   padding: EdgeInsets.only(left: 8.0),
                                                   child: Icon(Icons.check, size: 18),
@@ -1066,9 +1078,7 @@ class AudioPlayerScreenState extends State<AudioPlayerScreen> {
                                             ),
                                           );
                                         } else {
-                                          setState(() {
-                                            _repetitionMode.value = value;
-                                          });
+                                          _repetitionsMode.value = value;
                                         }
                                       },
                                     ),
@@ -1141,12 +1151,12 @@ class AudioPlayerScreenState extends State<AudioPlayerScreen> {
     );
   }
 
-  Future<void> _pause({bool analyticsOn = true}) async {
+  Future<void> _savePlayerPosition() async {
     final prefs = await SharedPreferences.getInstance();
     final positionData = await player.positionStream.first;
     final currentPosition = positionData.inMilliseconds;
     int currentIndex = player.currentIndex ?? 0;
-
+    print("will now save position: $currentPosition, player.currentIndex: ${player.currentIndex}");
     await prefs.setInt('savedPosition_${widget.documentID}_${widget.userID}', currentPosition);
     await prefs.setInt('savedTrackIndex_${widget.documentID}_${widget.userID}', currentIndex);
     await prefs.setBool("now_playing_${widget.documentID}_${widget.userID}", true);
@@ -1157,43 +1167,41 @@ class AudioPlayerScreenState extends State<AudioPlayerScreen> {
       nowPlayingList.add(widget.documentID);
     }
     await prefs.setStringList(nowPlayingKey, nowPlayingList);
+  }
 
+  Future<void> _pause({bool analyticsOn = true}) async {
+    _savePlayerPosition();
     player.pause();
-    if (mounted) {
-      setState(() {
-        isPlaying = false;
-        _isPaused = true;
-      });
-    }
+
     if (analyticsOn) {
       analyticsManager.storeAnalytics(widget.documentID, 'pause');
     }
   }
 
   Future<void> _play() async {
+    print("_play() called");
     final prefs = await SharedPreferences.getInstance();
     final savedPosition = prefs.getInt('savedPosition_${widget.documentID}_${widget.userID}');
     final savedTrackIndex = prefs.getInt('savedTrackIndex_${widget.documentID}_${widget.userID}');
 
-    setState(() {
-      isPlaying = true;
-      _isPaused = false;
-    });
-
+    print("savedPosition: $savedPosition, savedTrackIndex: $savedTrackIndex");
     if (savedPosition != null && savedTrackIndex != null) {
+      print("will now seek to position: $savedPosition, index: $savedTrackIndex");
       await player.seek(Duration(milliseconds: savedPosition), index: savedTrackIndex);
+      print("player.currentIndex: ${player.currentIndex}");
     }
     player.play();
 
     // Show ad for non-premium users every time they start playing
     if (!_hasPremium && !_hasShownInitialAd) {
       _hasShownInitialAd = true; // Prevent showing ad multiple times in same session
+      print("Now showing add");
       await AdService.showInterstitialAd(
         onAdShown: () async {
-          await _pause();
+          isPlaying.value = false;
         },
         onAdDismissed: () async {
-          await _play();
+          isPlaying.value = true;
         },
       );
     }
@@ -1216,9 +1224,8 @@ class AudioPlayerScreenState extends State<AudioPlayerScreen> {
 
     player.stop();
     player.seek(Duration.zero, index: 0);
+    isPlaying.value = false;
     setState(() {
-      isPlaying = false;
-      isStopped = true;
       currentTrack = script[0];
     });
   }
@@ -1240,10 +1247,8 @@ class AudioPlayerScreenState extends State<AudioPlayerScreen> {
 
   @override
   void dispose() {
-    _repetitionMode.dispose();
-    if (isPlaying) {
-      _stop();
-    }
+    _repetitionsMode.dispose();
+    isPlaying.dispose();
     firestoreService?.dispose();
     fileDurationUpdate?.dispose();
     player.dispose();
