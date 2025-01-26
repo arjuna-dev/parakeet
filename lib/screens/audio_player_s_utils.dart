06916f02b36a3579a66ec3e2791e100935996735
arjun-dev <arjuna1@protonmail.com>
2025-01-26 20:13:02 +0100

    Moved to use in both audio_player_screen and script_generator

---
+++ lib/screens/audio_player_s_utils.dart
@@ -0,0 +1,25 @@
+String accessBigJson(Map<String, dynamic> listWithBigJson, String filename) {
+  final pattern = RegExp(r'(\D+)|(\d+)');
+  final matches = pattern.allMatches(filename);
+
+  dynamic currentMap = listWithBigJson;
+  for (var match in matches) {
+    final key = match.group(0)!;
+    final cleanedKey = key.replaceAll(RegExp(r'^_|_$'), '');
+
+    if (int.tryParse(cleanedKey) != null) {
+      // If it's a number, parse it as an index
+      int index = int.parse(cleanedKey);
+      currentMap = currentMap[index];
+    } else {
+      // If it's not a number, use it as a string key
+      currentMap = currentMap[cleanedKey];
+    }
+
+    // If at any point currentMap is null, the key filename is invalid
+    if (currentMap == null) {
+      throw Exception("Invalid path: $filename");
+    }
+  }
+  return currentMap;
+}
