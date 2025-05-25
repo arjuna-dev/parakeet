6fee7a5b993372466d5058e7812dd682324f3fb7
arjun-dev <arjuna1@protonmail.com>
2025-05-25 15:42:59 +0200

    update breakpoints for responsive design in _MyAppState

--- lib/main.dart
+++ lib/main.dart
@@ -317,9 +317,9 @@ class _MyAppState extends State<MyApp> {
         builder: (context, child) => ResponsiveBreakpoints.builder(
               child: child!,
               breakpoints: [
-                const Breakpoint(start: 0, end: 450, name: MOBILE),
-                const Breakpoint(start: 451, end: 800, name: TABLET),
-                const Breakpoint(start: 801, end: 1920, name: DESKTOP),
+                const Breakpoint(start: 0, end: 599, name: MOBILE),
+                const Breakpoint(start: 600, end: 1023, name: TABLET),
+                const Breakpoint(start: 1024, end: 1920, name: DESKTOP),
                 const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
               ],
             ),
